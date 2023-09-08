a9676a0a5db6acccd9c39f6403c9c510419a906f
Vezora <143508209+Vezora-Corp@users.noreply.github.com>
2023-09-08 06:22:39 -0400

    Add files via upload

---
+++ Evaluate/EvalFunc_3.py
@@ -0,0 +1,24 @@
+import fire
+import sys
+
+from human_eval.data import HUMAN_EVAL
+from human_eval.evaluation import evaluate_functional_correctness
+
+def entry_point(
+    sample_file: str,
+    k: str = "1,10,100",
+    n_workers: int = 4,
+    timeout: float = 3.0,
+    problem_file: str = HUMAN_EVAL,
+):
+    """
+    Evaluates the functional correctness of generated samples, and writes
+    results to f"{sample_file}_results.jsonl.gz"
+    """
+    k = list(map(int, k.split(",")))
+    results = evaluate_functional_correctness(sample_file, k, n_workers, timeout, problem_file)
+    # ... rest of the code
+
+# This block ensures that the code only runs when executed as the main script
+if __name__ == '__main__':
+    fire.Fire(entry_point)
